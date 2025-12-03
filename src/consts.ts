aa8d811cb6bca5a0acccfdd7ba2a82677f3c4279
Stephen Suffian <stephen.suffian@pm.me>
2025-12-03 09:57:55 -0500

    Fix astro blog reference

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Philadelphia Area Jobs with Justice';
+export const SITE_DESCRIPTION = 'A coalition of labor unions and student, community, and faith groups fighting for fair treatment of working people in Philadelphia.';
