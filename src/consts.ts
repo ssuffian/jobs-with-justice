968aeea7dcb736b3efd952c7083c19168de47995
Stephen Suffian <stephen.suffian@pm.me>
2026-01-22 16:00:32 -0500

    Additional updates and redesign

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Philadelphia Area Jobs with Justice';
-export const SITE_DESCRIPTION = 'A coalition of labor unions and student, community, and faith groups fighting for fair treatment of working people in Philadelphia.';
+export const SITE_DESCRIPTION = 'Philadelphia\'s labor-community coalition building people power and working class power through solidarity. Power School organizer training, worker justice advocacy, and collective action for working people.';
